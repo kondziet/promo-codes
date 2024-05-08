8f93fb4e317c73a32c5f897dd37f1c4e878f3e40
Konrad Dąbrowski <86203980+kondziet@users.noreply.github.com>
2024-05-08 17:25:02 +0200

    Refactor DiscountStrategy

--- spring-backend/src/main/java/pl/kondziet/springbackend/domain/strategy/DiscountStrategy.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/domain/strategy/DiscountStrategy.java
@@ -1,9 +1,9 @@
 package pl.kondziet.springbackend.domain.strategy;
 
 import pl.kondziet.springbackend.domain.model.Money;
-import pl.kondziet.springbackend.domain.model.Product;
 
 public interface DiscountStrategy {
 
-    Money calculatePrice(Product product);
+    Money calculateDiscount(Money regularPrice);
+    Money calculateDiscountedPrice(Money initialPrice);
 }
