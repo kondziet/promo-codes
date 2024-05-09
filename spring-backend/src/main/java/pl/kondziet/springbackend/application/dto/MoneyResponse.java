50575d613e1ca7196af5d662f6637fa9e433323b
Konrad Dąbrowski <86203980+kondziet@users.noreply.github.com>
2024-05-09 18:35:02 +0200

    Add helper method

--- spring-backend/src/main/java/pl/kondziet/springbackend/application/dto/MoneyResponse.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/application/dto/MoneyResponse.java
@@ -1,4 +1,12 @@
 package pl.kondziet.springbackend.application.dto;
 
+import pl.kondziet.springbackend.domain.model.Money;
+
+import java.math.BigDecimal;
+
 public record MoneyResponse(Double amount, String currency) {
+
+    public Money toDomainObject() {
+        return new Money(Money.parseToValidAmount(BigDecimal.valueOf(amount)), currency);
+    }
 }
