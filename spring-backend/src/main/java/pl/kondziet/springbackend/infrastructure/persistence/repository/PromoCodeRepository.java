3a8d337ce1c6ff8de9dfdf892de53ecd5838576e
Konrad Dąbrowski <86203980+kondziet@users.noreply.github.com>
2024-05-09 18:41:40 +0200

    Add finding PromoCode by code

--- spring-backend/src/main/java/pl/kondziet/springbackend/infrastructure/persistence/repository/PromoCodeRepository.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/infrastructure/persistence/repository/PromoCodeRepository.java
@@ -1,7 +1,12 @@
 package pl.kondziet.springbackend.infrastructure.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.kondziet.springbackend.domain.model.PromoCode;
 
+import java.util.Optional;
+
 public interface PromoCodeRepository extends JpaRepository<PromoCode, Long> {
+
+    Optional<PromoCode> findByCode(String code);
 }
