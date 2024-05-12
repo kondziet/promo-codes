ae21c1815c8adf53c534570d2ebafc7cf28d71a1
kondziet <konrad.dabrowski54@gmail.com>
2024-05-12 14:09:57 +0200

    Remove nullable fields from JSON

--- spring-backend/src/main/java/pl/kondziet/springbackend/application/dto/ProductResponse.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/application/dto/ProductResponse.java
@@ -1,4 +1,7 @@
 package pl.kondziet.springbackend.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ProductResponse(String name, String description, MoneyResponse price) {
 }
