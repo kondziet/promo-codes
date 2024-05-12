bfe73803a6bd2df319fda94910dee14fba5235be
kondziet <konrad.dabrowski54@gmail.com>
2024-05-12 14:09:27 +0200

    Add helper method

--- spring-backend/src/main/java/pl/kondziet/springbackend/application/dto/MoneyRequest.java
+++ spring-backend/src/main/java/pl/kondziet/springbackend/application/dto/MoneyRequest.java
@@ -2,6 +2,7 @@ package pl.kondziet.springbackend.application.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import pl.kondziet.springbackend.domain.model.Money;
 
 public record MoneyRequest(
         @NotNull(message = "Amount is required")
@@ -9,4 +10,8 @@ public record MoneyRequest(
         @NotBlank(message = "Currency is required")
         String currency
 ) {
+
+    public Money toDomainObject() {
+        return new Money(amount, currency);
+    }
 }
